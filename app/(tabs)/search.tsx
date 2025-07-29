2c173cdba1c6f003e941919f6dcac88cad2be351
Vaios Anastasiadis <vaios_a@outlook.com>
2025-07-29 17:39:18 +0300

    Add bgs and images

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,42 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        height: "100%",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <ImageBackground
+        source={require("../../assets/images/bg.jpg")}
+        style={{ flex: 1, width: "100%", height: "100%" }}
+      >
+        <View style={styles.viewClass}>
+          <Image
+            style={styles.logoClass}
+            source={require("../../assets/images/logo.png")}
+          />
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
   );
 };
 
 export default Search;
+
+const styles = StyleSheet.create({
+  viewClass: {
+    height: 300,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoClass: {
+    width: 100,
+    height: 50,
+  },
+});
